5386b3b42f1c573b7724171b3b16d374afa26acf
Your Name <you@example.com>
2020-12-28 09:58:05 +0530

    ..

--- photograph.java
+++ photograph.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-public class D {
+public class photograph {
     //
     public static void main(String[] args) {
         FastScanner sc=new FastScanner();
